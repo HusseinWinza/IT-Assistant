e4891582127d9900a8bf2035eefb007d833e8074
Hussein Winza <husseinwinza@gmail.com>
2019-05-20 12:01:04 +0300

    Create constants.php
    added api

---
+++ API/constants.php
@@ -0,0 +1,6 @@
+<?php
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'id3068529_muchunguzi6262');
+	define('DB_PASSWORD', 'muchunguzi6262');
+	define('DB_NAME', 'id3068529_dictdatabase');
+?>
