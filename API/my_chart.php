88c5f7ce5b9f1771fc3aa839923697a281a5acc6
Hussein Winza <husseinwinza@gmail.com>
2019-05-20 12:01:38 +0300

    Create my_chart.php
    added api

---
+++ API/my_chart.php
@@ -0,0 +1,40 @@
+ <?php
+  require_once "connect.php";
+
+  if(!$con){
+    echo "Database connected failed";
+  }
+  else{
+  
+     
+               if($_SERVER['REQUEST_METHOD'] == "POST")
+               {
+
+                   
+              $from1 = $_POST['from1'];
+              $status = $_POST['status'];
+                
+          
+                              $dupe_status = "SELECT * FROM chart WHERE from1 = '$from1' AND status = '$status'";
+                              $dupe_status_results = mysqli_query($con, $dupe_status);
+                              $response = array();
+
+                              if(!$dupe_status_results){
+                                    echo "*0*";
+                              }else{
+                                while ($row=mysqli_fetch_array($dupe_status_results)) {
+                                array_push($response, array("id"=>$row[0], "to"=>$row[1], "from1"=>$row[2], "message"=>$row[3], "id_message"=>$row[5]));
+                                }
+                                  print(json_encode(array("server_response"=>$response)));
+                              }
+
+                    mysqli_close($con);
+                     
+
+               }else{
+            echo "Improper Request Method";
+              }
+
+       }
+   
+?>
